d834d7b50982e3bdfadcb619b1c2879d935116f1
tinganchen <anna910003@gmail.com>
2021-10-19 18:38:25 +0800

    modify dropbox url and final test

--- hw1/hw1_1.sh
+++ hw1/hw1_1.sh
@@ -4,9 +4,9 @@ cd ./p1
 
 mkdir best_model/
 
-wget https://www.dropbox.com/s/x4guurxmcvhtpsp/model_best.pt
+wget https://www.dropbox.com/s/x4guurxmcvhtpsp/model_best.pt?dl=1
 
-mv model_best.pt best_model/model_best.pt
+mv model_best.pt?dl=1 best_model/model_best.pt
 
 python3 inference.py --bash_path $CURRENT_PATH --test_dataset $1 --output_file $2 --test_only True --tsne False --pretrained True --ground_truth False
 
--- hw1/hw1_2.sh
+++ hw1/hw1_2.sh
@@ -11,11 +11,11 @@ mkdir pretrain/
 mkdir pretrain/resnet110_cifar100
 
 # Download model weights
-wget https://www.dropbox.com/s/9iqdgdwm0a0qhtk/model_best.pt
-mv model_best.pt best_model/fcn_resnet50/model_best.pt
+wget https://www.dropbox.com/s/9iqdgdwm0a0qhtk/model_best.pt?dl=1
+mv model_best.pt?dl=1 best_model/fcn_resnet50/model_best.pt
 
-wget https://www.dropbox.com/s/sb4jbd99xwj4fny/model_best.pt
-mv model_best.pt pretrain/resnet110_cifar100/model_best.pt
+wget https://www.dropbox.com/s/sb4jbd99xwj4fny/model_best.pt?dl=1
+mv model_best.pt?dl=1 pretrain/resnet110_cifar100/model_best.pt
 
 # inference
 python3 inference.py --bash_path $CURRENT_PATH --test_dataset $1 --output_dir $2 --test_only True --pretrained True
